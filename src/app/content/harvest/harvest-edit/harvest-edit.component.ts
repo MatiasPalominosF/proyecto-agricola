3d930e393ac58668079504a598f17fec9ee79ab6
Matías <matiiias.-15@live.cl>
2021-09-30 18:56:39 -0300

    Editar registro ok

--- src/app/content/harvest/harvest-edit/harvest-edit.component.ts
+++ src/app/content/harvest/harvest-edit/harvest-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { HarvestService } from 'src/app/_services/harvest/harvest.service';
 
 @Component({
   selector: 'app-harvest-edit',
@@ -7,9 +10,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HarvestEditComponent implements OnInit {
 
-  constructor() { }
+  @Input() public idCategory: string;
+  @Input() public idUser: string;
+  @Input() public weight: number;
+  @Input() public id: string;
+  @Input() public acumulate: number;
+  @Output() passEntry: EventEmitter<any> = new EventEmitter();
+
+  public registerInfo: FormGroup;
+  public submitted = false;
+  private restante = 0;
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private harvestService: HarvestService,
+  ) { }
 
   ngOnInit(): void {
+    this.registerInfo = this.formBuilder.group({
+      weight: ['', [Validators.required, Validators.min(1)]],
+    });
+
+    this.setDataInForm();
   }
 
+  setDataInForm() {
+    this.weight = this.round(this.weight, 2)
+    this.f['weight'].setValue(this.weight);
+  }
+
+  get f() {
+    return this.registerInfo.controls;
+  }
+
+  get fValue() {
+    return this.registerInfo.value;
+  }
+
+  keyPress(event: any) {
+    const pattern = /([0-9\+\-\+\.])/;
+
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode !== 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.registerInfo.get(controlName).hasError(errorName);
+  };
+
+  round(value, exp) {
+    if (typeof exp === 'undefined' || +exp === 0)
+      return Math.round(value);
+
+    value = +value;
+    exp = +exp;
+
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+      return NaN;
+
+    // Shift
+    value = value.toString().split('e');
+    value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+  }
+
+  updateReg(acumulate: number, weight: number): void {
+    this.harvestService.updateFieldInRegisterUsers(this.idCategory, this.idUser, acumulate);
+    this.harvestService.updateFieldInRegisters(this.idCategory, this.idUser, this.id, weight);
+  }
+
+
+  onFormSubmit() {
+    this.submitted = true;
+
+
+    if (this.registerInfo.invalid) {
+      return;
+    }
+
+    var weightFlag = +this.f.weight.value;
+    this.restante = this.weight - +this.f.weight.value;
+
+    if (this.restante > 0) {
+      weightFlag = 0;
+      this.restante = this.restante * -1;
+      this.acumulate += this.restante;
+      this.updateReg(this.acumulate, +this.f.weight.value);
+      this.passEntry.emit(true);
+      this.activeModal.close(true);
+    }
+    else if (this.restante < 0) {
+      weightFlag = 0;
+      this.restante = this.restante * -1;
+      this.acumulate += this.restante;
+      this.updateReg(this.acumulate, +this.f.weight.value);
+      this.passEntry.emit(true);
+      this.activeModal.close(true);
+    }
+    else if (this.restante == 0) {
+      this.restante = weightFlag * -1;
+      weightFlag = 0;
+      this.acumulate += this.restante;
+      this.updateReg(this.acumulate, +this.f.weight.value);
+      this.passEntry.emit(true);
+      this.activeModal.close(true);
+    }
+  }
 }
--- src/app/content/harvest/registers-users/registers-users.component.ts
+++ src/app/content/harvest/registers-users/registers-users.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ConfirmationService } from 'src/app/_services/confirmation/confirmation.service';
 import { HarvestService } from 'src/app/_services/harvest/harvest.service';
 import { NotificationService } from 'src/app/_services/notification/notification.service';
 import { RegisterUser } from '../../../_models/register-user';
+import { HarvestEditComponent } from '../harvest-edit/harvest-edit.component';
 
 @Component({
   selector: 'app-registers-users',
@@ -19,6 +20,7 @@ export class RegistersUsersComponent implements OnInit {
   @Input() public nameUser: string;
   @BlockUI('userRegister') blockUIHarvest: NgBlockUI;
 
+  private closeResult = '';
   public categoryName: string;
   public rol: boolean;
   private registersUsers: RegisterUser[];
@@ -27,6 +29,7 @@ export class RegistersUsersComponent implements OnInit {
 
   constructor(
     public activeModal: NgbActiveModal,
+    private modalService: NgbModal,
     private harvestService: HarvestService,
     private confirmationDialogService: ConfirmationService,
     private notifyService: NotificationService,
@@ -48,8 +51,32 @@ export class RegistersUsersComponent implements OnInit {
     });
   }
 
-  editRegister(id: string) {
-    console.log("Se edita registro ID: " + id);
+  editRegister(id: string, weight: number) {
+    weight = +weight;
+    const modalRef = this.modalService.open(HarvestEditComponent, { windowClass: 'animated fadeInDown', size: 'sm', backdrop: 'static' });
+    modalRef.componentInstance.weight = weight;
+    modalRef.componentInstance.idCategory = this.category;
+    modalRef.componentInstance.idUser = this.id;
+    modalRef.componentInstance.id = id;
+    modalRef.componentInstance.acumulate = this.acumulate;
+    modalRef.result.then((result) => {
+      if (result) {
+        this.notifyService.showSuccess("Editar", "¡El registro se editó correctamente!");
+      }
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      console.log(this.closeResult);
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   updateAcumulate(weight: number): void {
