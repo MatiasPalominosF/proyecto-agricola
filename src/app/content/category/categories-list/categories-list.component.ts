a6be921a748d3cdeecd13b254f85b1046c8d3bed
Matías <matiiias.-15@live.cl>
2021-11-23 20:39:40 -0300

    Se muestran los datos correspondientes en categorías

--- src/app/content/category/categories-list/categories-list.component.ts
+++ src/app/content/category/categories-list/categories-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Harvest } from 'src/app/_models/harvest';
+import { HarvestService } from 'src/app/_services/harvest/harvest.service';
 
 @Component({
   selector: 'app-categories-list',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesListComponent implements OnInit {
 
-  public breadcrumb: any;
+  @BlockUI('categoriesCard') blockUICategories: NgBlockUI;
 
-  constructor() { }
+  public breadcrumb: any;
+  private currentUser: any;
+  public options = {
+    close: false,
+    expand: true,
+    minimize: true,
+    reload: true
+  };
+  public headElements = ['#', 'Categoría', 'Fecha inicio', 'Fecha término', 'Acciones'];
+  private harvests: Harvest[];
+  public collectionSize: any;
+  public pipe: any;
+  public page = 1;
+  public pageSize = 4;
+  public harvestSearch: Observable<Harvest[]>;
+  public filter = new FormControl('');
+
+
+  constructor(
+    private harvestService: HarvestService,
+  ) { }
 
   ngOnInit(): void {
     this.breadcrumb = {
@@ -28,6 +55,56 @@ export class CategoriesListComponent implements OnInit {
       ],
       'options': false
     };
+    this.getUserLogged();
+    this.getFullInfoHarvest();
+  }
+
+  getFullInfoHarvest(): void {
+    this.blockUICategories.start("Cargando...");
+    this.harvestService.getFullInfoHarvest().subscribe(data => {
+      console.log("Data:", data);
+      this.harvests = data;
+      this.collectionSize = this.harvests.length;
+      this.searchData(this.pipe);
+      this.blockUICategories.stop();
+    });
+  }
+
+  /**
+  *
+  * '@param' pipe
+  */
+  searchData(pipe: DecimalPipe) {
+    this.harvestSearch = this.filter.valueChanges.pipe(
+      startWith(''),
+      map(text => this.search(text, pipe))
+    );
+  }
+
+  /**
+   * Search table
+   * '@param' text
+   * '@param' pipe
+   */
+  search(text: string, pipe: PipeTransform) {
+    return this.harvests.filter(response => {
+      const term = text.toLowerCase();
+      return response.name.toLowerCase().includes(term)
+    });
+  }
+
+  getUserLogged(): void {
+    if (localStorage.getItem('currentUser')) {
+      this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+  }
+
+  reload(event: any): void {
+    this.blockUICategories.start('Cargando...');
+
+    setTimeout(() => {
+      this.blockUICategories.stop();
+    }, 2500);
   }
 
 }
--- src/app/content/category/category.module.ts
+++ src/app/content/category/category.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { RouterModule } from '@angular/router';
 import { BreadcrumbModule } from 'src/app/_layout/breadcrumb/breadcrumb.module';
+import { CardModule } from '../partials/general/card/card.module';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockTemplateComponent } from 'src/app/_layout/blockui/block-template.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PipeModule } from 'src/app/_pipe/pipe.module';
 
 
 
@@ -13,6 +19,15 @@ import { BreadcrumbModule } from 'src/app/_layout/breadcrumb/breadcrumb.module';
   imports: [
     CommonModule,
     BreadcrumbModule,
+    CardModule,
+    NgbModule,
+    NgbModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PipeModule,
+    BlockUIModule.forRoot({
+      template: BlockTemplateComponent
+    }),
     RouterModule.forChild([
       {
         path: 'categories-list',
