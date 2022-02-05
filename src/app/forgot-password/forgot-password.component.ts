545bd5c462d15460c15dc1faff2e8b9cd209b44d
Matías <matiiias.-15@live.cl>
2022-02-05 12:14:54 -0300

    Vista recuperar contraseña
    Se crea una vista para recuperar la contraseña

---
+++ src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    templateUrl: 'forgot-password.component.html',
+    styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+    }
+
+}
