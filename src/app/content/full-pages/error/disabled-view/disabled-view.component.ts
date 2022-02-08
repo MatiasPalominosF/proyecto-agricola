40093c74701f070b5c2a65779f7f6dcc862e1990
Matías <matiiias.-15@live.cl>
2022-02-08 20:25:17 -0300

    Vista usuario desactivado ok

--- src/app/content/full-pages/error/disabled-view/disabled-view.component.ts
+++ src/app/content/full-pages/error/disabled-view/disabled-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 import { UserInterface } from 'src/app/_models/user';
 
 @Component({
@@ -8,23 +9,64 @@ import { UserInterface } from 'src/app/_models/user';
 })
 export class DisabledViewComponent implements OnInit {
   public emailString: string;
+  public wtspString: string;
   public url: string;
   private currentUser: UserInterface;
-  constructor() { }
+  public isCompany: boolean;
+  private urlwtsp: string;
+  private mensaje: string;
+  constructor(
+    private deviceService: DeviceDetectorService,
+  ) { }
 
   ngOnInit(): void {
+    this.getInfo();
+  }
+
+  getInfo(): void {
+    this.urlwtsp = "";
+    this.mensaje = "";
     this.currentUser = this.getCurrentUser;
-    console.log(this.currentUser);
+
     if (this.currentUser) {
-      if (this.currentUser.rol === 'worker') {
-        this.url = "/harvest/harvests-view";
-      } else {
-        this.url = "/dashboard/show-data"
-      }
+      this.getUrl();
+      this.getUrlWtsp();
       this.emailString = `mailto:palominos90@gmail.com?Subject=Usuario ${this.currentUser.firstName} (rut:${this.currentUser.run}) desactivado`;
     } else {
       this.emailString = "mailto:palominos90@gmail.com?Subject=Usuario desactivado";
     }
+  }
+
+  getUrl() {
+    this.isCompany = this.isOwner;
+    if (this.currentUser.rol === 'worker') {
+      this.url = "/harvest/harvests-view";
+    } else {
+      this.url = "/dashboard/show-data"
+    }
+  }
+
+  getUrlWtsp() {
+    if (this.isDesktop) {
+      this.urlwtsp = "https://web.whatsapp.com/";
+    } else if (this.isMobile || this.isTablet) {
+      this.urlwtsp = "whatsapp://"
+    }
+
+    this.mensaje = `send?phone=56989189474&text=*Usuario ${this.currentUser.firstName} (rut: ${this.currentUser.run}) desactivado*%0A`;
+    this.wtspString = this.urlwtsp + this.mensaje;
+  }
+
+  get isMobile(): boolean {
+    return this.deviceService.isMobile();
+  }
+
+  get isTablet(): boolean {
+    return this.deviceService.isTablet();
+  }
+
+  get isDesktop(): boolean {
+    return this.deviceService.isDesktop();
   }
 
   get getCurrentUser(): UserInterface {
@@ -33,7 +75,10 @@ export class DisabledViewComponent implements OnInit {
     }
   }
 
-  sendEmail(): void {
-
+  get isOwner(): boolean {
+    if (this.currentUser.rol === 'company') {
+      return true;
+    }
+    return false;
   }
 }
